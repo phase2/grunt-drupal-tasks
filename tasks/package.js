400ef7cb2d3d10ed7216907844630230ae63338c
Joe Turgeon <jturgeon@phase2technology.com>
2014-06-18 15:33:20 -0500

    Adding Gruntconfig.json options for the packages output directory, source and project files to include in packages.

--- tasks/package.js
+++ tasks/package.js
@@ -7,6 +7,9 @@ module.exports = function(grunt) {
    *   Builds a deployment package in the build/package directory.
    */
   grunt.loadTasks(__dirname + '/../node_modules/grunt-contrib-compress/tasks');
+  var config = grunt.config.get('config'),
+    srcFiles = (config.packages && config.packages.srcFiles && config.packages.srcFiles.length) ? config.packages.srcFiles : [],
+    projFiles = (config.packages && config.packages.projFiles && config.packages.projFiles.length) ? config.packages.projFiles : [];
   grunt.config('compress', {
     package: {
       options: {
@@ -16,8 +19,16 @@ module.exports = function(grunt) {
         mode: 'tgz'
       },
       files: [
-        {expand: true, cwd: '<%= config.buildPaths.html %>', src: ['**', '!sites/*/files/**', '!xmlrpc.php', '!modules/php/*'], dest: 'docroot/'},
-        {expand: true, src: ['README*', 'bin/**']}
+        {
+          expand: true,
+          cwd: '<%= config.buildPaths.html %>',
+          src: ['**'].concat(srcFiles),
+          dest: 'docroot/'
+        },
+        {
+          expand: true,
+          src: projFiles
+        }
       ]
     }
   });
