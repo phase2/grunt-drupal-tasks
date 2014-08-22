4116c2c49e5f80aa58868bf83e4e092d6099db79
Adam Ross <aross@phase2technology.com>
2014-08-22 01:19:44 -0400

    Fix compass compilation as part of default build process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,8 +24,8 @@ module.exports = function(grunt) {
   if (grunt.config.get(['composer', 'install'])) {
     tasksDefault.unshift('composer:install');
   }
-  if (grunt.config.get('compass')) {
-    tasksDefault.push('compass');
+  if (grunt.task.exists('compile-theme')) {
+    tasksDefault.push('compile-theme');
   }
   if (grunt.config.get('behat')) {
    // tasksDefault.push('behat');
