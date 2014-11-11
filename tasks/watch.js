eefdaae9e69859c3fd1e97805dd37c6380662bbc
Grayside <aross@phase2technology.com>
2014-11-11 15:17:27 -0800

    #13: Initial use of grunt-parallel to unstick watch. [WIP]

--- tasks/watch.js
+++ tasks/watch.js
@@ -25,7 +25,48 @@ module.exports = function(grunt) {
     tasks: ['validate']
   });
 
-  grunt.config('help.watch', {
+  // Watch SASS files for changes, if there is run compile-theme task.
+  // A more fine-grained task may be needed later if more non-SASS elements are
+  // added to the compile-theme task.
+  grunt.config(['watch', 'compass'], {
+    files: [
+      '<%= config.srcPaths.drupal %>/themes/**/sass/*scss'
+    ],
+    tasks: ['compile-theme']
+  });
+
+  grunt.config(['parallel', 'watch-test'], {
+    tasks: [
+      {
+        grunt: true,
+        stream: true,
+        args: ['watch:validate']
+      },
+      {
+        grunt: true,
+        stream: true,
+        args: ['watch:behat']
+      }
+    ]
+  });
+
+  grunt.config(['parallel', 'watch-theme'], {
+    tasks: [
+      {
+        grunt: true,
+        stream: true,
+        args: ['watch:compass']
+      }
+    ]
+  });
+
+  grunt.registerTask('watch-test', ['parallel:watch-test']);
+  grunt.registerTask('watch-theme', ['parallel:watch-theme']);
+
+  grunt.config('watch-test', {
+    group: 'Real-time Tooling'
+  });
+  grunt.config('watch-theme', {
     group: 'Real-time Tooling'
   });
 }
