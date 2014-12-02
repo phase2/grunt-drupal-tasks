9bf0604aaac5a3b4e4d6b479c387368e9424391c
Joe Turgeon <jturgeon@phase2technology.com>
2014-12-01 19:41:05 -0600

    Adding comments and fixing gitignore paths.

--- test/create_working_copy.sh
+++ test/create_working_copy.sh
@@ -5,9 +5,16 @@
 
 if [ ! -f "Gruntfile.js" ]; then echo "This script should be run from the grunt-drupal-tasks directory."; exit 1; fi;
 
+# Initialize the working_copy directory
 rm -rf test/working_copy
 mkdir test/working_copy
+
+# Copy the example skeleton into working_copy
 cp -r example/* test/working_copy
+
+# Install dependencies of working_copy
 cd test/working_copy
 npm install
+
+# Force installation of the checked out version of grunt-drupal-tasks
 npm install ../..
