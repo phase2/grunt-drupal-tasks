e7333b445c88012743e0014ace4b629e0cec06d6
Adam Ross <grayside@gmail.com>
2014-12-13 14:09:46 -0800

    Clarify require target in Gruntfile warning message

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
     // We may want to use grunt to build our grunt tools one day.
-    grunt.log.errorlns('Please require `grunt-drupal-tasks/bootstrap` in your Gruntfile.');
+    grunt.log.errorlns('Please require `grunt-drupal-tasks` in your Gruntfile.');
     grunt.log.errorlns('Direct use of `grunt-drupal-tasks/Gruntfile.js` is deprecated.');
     require('./bootstrap')(grunt);
 }
