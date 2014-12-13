ab0f006f41f3a8cc1038c640c00ab09095f7203d
Grayside <aross@phase2technology.com>
2014-12-13 00:10:57 -0800

    Deprecate Gruntfile.js in favor of bootstrap.js

--- example/Gruntfile.js
+++ example/Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
   // Load all plugins and tasks defined by the grunt-drupal-tasks package.
-  require('grunt-drupal-tasks/Gruntfile')(grunt);
+  require('grunt-drupal-tasks/bootstrap')(grunt);
 
 };
