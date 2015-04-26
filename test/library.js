f84255b25d370eefb464d9ef25919fc0a3536ca5
Grayside <aross@phase2technology.com>
2015-04-26 08:13:14 -0700

    Correct style errors and further adjust to code changes since merge.

--- test/library.js
+++ test/library.js
@@ -1,21 +1,21 @@
 var assert = require('assert');
-var GDT = require('../lib/gdt') ;
+var GDT = require('../lib/util') ;
 
 describe('grunt-drupal-tasks/util.js', function() {
   describe('provides a capability to construct absolute URLs', function() {
-  	it ('allows expansion of a root path', function() {
-    	var items = GDT.expandUrls('http://example.com', ['/']);
-    	assert.equal('http://example.com/', items[0]);
-	});
-  	it ('allows expansion of multiple paths', function() {
-    	var items = GDT.expandUrls('http://example.com', ['/a', '/b']);
-    	assert.equal('http://example.com/a', items[0]);
-    	assert.equal('http://example.com/b', items[1]);
-	});
-  	it ('allows expansion of a base URL with a base path', function() {
-    	var items = GDT.expandUrls('http://example.com/subsite', ['/a', '/b']);
-    	assert.equal('http://example.com/subsite/a', items[0]);
-    	assert.equal('http://example.com/subsite/b', items[1]);
-	});
+    it ('allows expansion of a root path', function() {
+      var items = GDT.expandUrls('http://example.com', ['/']);
+      assert.equal('http://example.com/', items[0]);
+    });
+    it ('allows expansion of multiple paths', function() {
+      var items = GDT.expandUrls('http://example.com', ['/a', '/b']);
+      assert.equal('http://example.com/a', items[0]);
+      assert.equal('http://example.com/b', items[1]);
+    });
+    it ('allows expansion of a base URL with a base path', function() {
+      var items = GDT.expandUrls('http://example.com/subsite', ['/a', '/b']);
+      assert.equal('http://example.com/subsite/a', items[0]);
+      assert.equal('http://example.com/subsite/b', items[1]);
+    });
   })
 })
