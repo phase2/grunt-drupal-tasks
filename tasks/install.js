93ed9b6ddc3097a461a121179e9f1b186a35e3fe
Jonathan Hedstrom <jhedstrom@gmail.com>
2015-08-31 15:12:30 -0700

    Initial work on an install task.
    - Task works if no database is specified
    - In manual testing, loaddb doesn't load the full database

---
+++ tasks/install.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+  /**
+   * Define tasks to quickly get Drupal running.
+   *
+   * grunt serve
+   *   Run Drupal via the PHP webserver and activate watch tasks.
+   * grunt serve:demo
+   *   Run Drupal via the PHP webserver without running watch tasks.
+   * grunt serve:test
+   *   Run Drupal via the PHP webserver without running watch tasks or opening
+   *   the browser.
+   */
+  grunt.loadNpmTasks('grunt-drush');
+
+  var Help = require('../lib/help')(grunt),
+    Drupal = require('../lib/drupal')(grunt);
+
+  var path = require('path'),
+    _ = require('lodash');
+
+  // If no path is configured for Drush, fallback to the system path.
+  var cmd = {cmd: Drupal.drushPath()};
+
+  var profile = grunt.config('config.serve.profile') || 'standard';
+
+  grunt.config(['drush', 'install'], {
+    args: ['site-install', '-yv', profile],
+    options: _.extend({
+      cwd: '<%= config.buildPaths.html %>'
+    }, cmd)
+  });
+  grunt.config(['drush', 'sql-drop'], {
+    args:['sql-drop', '-y'],
+    options: _.extend({
+      cwd: '<%= config.buildPaths.html %>'
+    }, cmd)
+  });
+  grunt.config(['shell', 'loaddb'], {
+    command: 'gzip -dc <%= config.install.db %> | <%= config.install.dbConnection %>'
+  });
+
+  grunt.registerTask('install', 'Install Drupal with the configured profile, or from a database dump file if one exists.', function() {
+
+    var done = this.async();
+
+    // Check for a database file first.
+    var dbPath = grunt.config('config.install.db');
+    if (dbPath && grunt.file.exists(dbPath)) {
+      Drupal.loadDatabaseConnection(function(error) {
+        if (error) {
+          grunt.fail.fatal(error);
+          done();
+        }
+        grunt.task.run([
+          // Drop current database.
+          'drush:sql-drop',
+          // Load new database.
+          'shell:loaddb'
+        ]);
+        done();
+      });
+    }
+    else {
+      grunt.task.run('drush:install');
+      done();
+    }
+  });
+
+  Help.add([
+    {
+      task: 'install',
+      group: 'Operations'
+    },
+  ]);
+};
