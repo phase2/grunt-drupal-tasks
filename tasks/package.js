f01ee024d97dfce21812b41c93d4948b9975d31f
eric paul <epaul@phase2technology.com>
2015-10-22 21:53:01 -0700

    Initial swing and proof of concept for acquia packaging.

--- tasks/package.js
+++ tasks/package.js
@@ -13,35 +13,110 @@ module.exports = function(grunt) {
     var path = require('path');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    var config = grunt.config.get('config.packages'),
+    var config = grunt.config.get('config.packages.default'),
       srcFiles = ['**', '!**/.gitkeep'].concat((config && config.srcFiles && config.srcFiles.length) ? config.srcFiles : '**'),
       projFiles = (config && config.projFiles && config.projFiles.length) ? config.projFiles : [];
 
     var destPath = grunt.config.get('config.buildPaths.package') + '/package';
     var tasks = []
 
-    grunt.config('copy.package', {
-      files: [
-        {
-          expand: true,
-          cwd: '<%= config.buildPaths.html %>',
-          src: srcFiles,
-          dest: destPath + (grunt.config.get('config.packages.dest.docroot') || ''),
-          dot: true
-        },
-        {
-          expand: true,
-          src: projFiles,
-          dest: destPath + (grunt.config.get('config.packages.dest.devResources') || ''),
-          dot: true
-        }
-      ],
-      options: {
-        gruntLogHeader: false
-      }
-    });
+    // Figure out if we are building a vendor-specific package.
+    var buildTarget = grunt.option('target') || process.env.GRUNT_PACKAGE_TARGET;
+    if (buildTarget && isValidTarget(buildTarget)) {
+      grunt.config('config.buildTarget', grunt.option('target'))
+    }
+    else {
+      grunt.config('config.buildTarget', 'default');
+    }
+    grunt.log.writeln('Building package for ' + grunt.config.get('config.buildTarget'));
 
-    tasks.push('copy:package');
+    var validTarget = grunt.config.get('config.buildTarget');
+
+    // Build the specified vendor target, or the default.
+    switch (validTarget) {
+      case 'acquia':
+        /**
+         * Acquia release build tasks.
+         */
+        grunt.config.merge({
+          copy: { 'acquia_docroot': {
+            files: [
+              {
+                expand: true,
+                cwd: '<%= config.buildPaths.html %>',
+                src: '<%= config.packages.acquia.srcFiles %>',
+                dest: '<%= config.packages.acquia.paths.docroot %>'
+              }
+            ]
+          }}
+        });
+        grunt.config.merge({
+          copy: { 'acquia_shared': {
+            files: [
+              {
+                expand: true,
+                cwd: '<%= config.buildPaths.html %>/sites/default',
+                src: '<%= config.packages.acquia.sharedFiles %>',
+                dest: '<%= config.packages.acquia.paths.shared %>'
+              }
+            ]
+          }}
+        });
+
+        /**
+         * Acquia clean tasks.
+         */
+        grunt.config.merge({
+          clean: {
+            'acquia_docroot': '<%= config.packages.acquia.paths.docroot %>',
+            'acquia_shared': '<%= config.packages.acquia.paths.shared %>'
+          }
+        });
+
+        /**
+         * Symlink shared files (settings, etc) that is not part of docroot.
+         */
+        grunt.loadNpmTasks('grunt-contrib-symlink');
+        grunt.config(['symlink', 'shared'], {
+          expand: true,
+          cwd: '<%= config.packages.acquia.paths.shared %>',
+          src: ['*'],
+          dest: '<%= config.packages.acquia.paths.docroot %>/sites/default/'
+        });
+
+        tasks.push('clean:acquia_docroot');
+        tasks.push('clean:acquia_shared');
+        tasks.push('copy:acquia_docroot');
+        tasks.push('copy:acquia_shared');
+        tasks.push('symlink:shared');
+
+        break;
+
+      default:
+        grunt.config('copy.package', {
+          files: [
+            {
+              expand: true,
+              cwd: '<%= config.buildPaths.html %>',
+              src: srcFiles,
+              dest: destPath + (grunt.config.get('config.packages.dest.docroot') || ''),
+              dot: true
+            },
+            {
+              expand: true,
+              src: projFiles,
+              dest: destPath + (grunt.config.get('config.packages.dest.devResources') || ''),
+              dot: true
+            }
+          ],
+          options: {
+            gruntLogHeader: false
+          }
+        });
+
+        tasks.push('copy:package');
+
+    }
 
     if (this.args[0] && this.args[0] == 'compress') {
       grunt.loadNpmTasks('grunt-contrib-compress');
@@ -71,4 +146,15 @@ module.exports = function(grunt) {
     task: 'package',
     group: 'Operations'
   });
+
+  function isValidTarget(target) {
+    if (target.length) {
+      var validTargets = ['acquia'];
+      return (validTargets.indexOf(target) > -1);
+    }
+    else {
+      return false;
+    }
+  }
+
 };
