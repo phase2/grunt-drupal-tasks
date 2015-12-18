5e4ccea2f4b7948b4b9c423c13e666a50b8b6eb3
Jonathan Hedstrom <jhedstrom@gmail.com>
2015-12-18 12:09:28 -0800

    Properly detect Drupal version.

--- tasks/test.js
+++ tasks/test.js
@@ -3,11 +3,6 @@ module.exports = function(grunt) {
   var Drupal = require('../lib/drupal')(grunt);
   var Help = require('../lib/help')(grunt);
 
-  // Default to Drupal 8 unless Drupal 7 is explicitly defined.
-  var script = Drupal.majorVersion() == 7 ? './scripts/run-tests.sh' : './core/scripts/run-tests.sh';
-  var testLocation = Drupal.majorVersion() == 7 ? './sites/all/modules/custom' : './modules/custom';
-  var domain = process.env.GDT_DOMAIN || grunt.config('config.domain') || require('os').hostname();
-
   grunt.config('test', {
     behat: true,
     drupal: {
@@ -15,26 +10,43 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.config(['shell', 'testdrupal'], {
-    command: grunt.config.get('config.testing.drupal.command') || 'php ' + script + ' --verbose --color --concurrency 4 --directory ' + testLocation + ' --url ' + domain,
-    options: {
-      execOptions: {
-        cwd: '<%= config.buildPaths.html %>'
-      }
-    }
-  });
-
   // @todo Would rather display as 'alias for test:behat, test:drupal, etc'.
   grunt.registerMultiTask('test', 'Run all configured tests.', function(args) {
     var self = this;
-    switch (self.target) {
-      case 'behat':
-        grunt.task.run('behat');
-        break;
+    var done = this.async();
 
-      case 'drupal':
-        grunt.task.run('shell:testdrupal');
-    }
+    Drupal.loadDrushStatus(function(err) {
+      if (err) {
+        grunt.fail.fatal('Cannot load Drush status for built Drupal docroot.\n\n');
+        return done();
+      }
+
+      var script = Drupal.majorVersion() == 7 ? './scripts/run-tests.sh' : './core/scripts/run-tests.sh';
+      var testLocation = Drupal.majorVersion() == 7 ? './sites/all/modules/custom' : './modules/custom';
+      var domain = process.env.GDT_DOMAIN || grunt.config('config.domain') || require('os').hostname();
+
+      grunt.config(['shell', 'testdrupal'], {
+        command: grunt.config.get('config.testing.drupal.command') || 'php ' + script + ' --verbose --color --concurrency 4 --directory ' + testLocation + ' --url ' + domain,
+        options: {
+          execOptions: {
+            cwd: '<%= config.buildPaths.html %>'
+          }
+        }
+      });
+
+      switch (self.target) {
+        case 'behat':
+          grunt.task.run('behat');
+          break;
+
+        case 'drupal':
+          grunt.task.run('shell:testdrupal');
+          break;
+      }
+
+      done();
+    });
+
   });
 
   Help.add({
