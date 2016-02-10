49560ef1cf19027dbc7eb168a8e40b46756f56d4
Joe Turgeon <jturgeon@phase2technology.com>
2016-02-09 18:11:50 -0600

    Re-implement the 'force' task prefix for eslint when the 'config.validate.ignoreError' config is set.

--- tasks/quality.js
+++ tasks/quality.js
@@ -128,9 +128,7 @@ module.exports = function(grunt) {
           '!<%= config.srcPaths.drupal %>/sites/**/files/**/*.js'
         ],
       eslintTargetAnalyze = eslintTarget,
-      eslintConfigFile = eslintConfig.configFile || './.eslintrc',
-      eslintIgnoreError = grunt.config.get('config.validate.ignoreError') === undefined ? false : grunt.config.get('config.validate.ignoreError'),
-      eslintName = eslintIgnoreError ? 'force:eslint' : 'eslint';
+      eslintConfigFile = eslintConfig.configFile || './.eslintrc';
 
     for (var key in themes) {
       if (themes[key].scripts && themes[key].scripts.validate) {
@@ -186,10 +184,12 @@ module.exports = function(grunt) {
         validate.push('phpcs:validate');
       }
     }
-    var eslint = grunt.config.get('eslint.validate');
+    var eslint = grunt.config.get('eslint.validate'),
+      eslintIgnoreError = grunt.config.get('config.validate.ignoreError') === undefined ? false : grunt.config.get('config.validate.ignoreError'),
+      eslintName = eslintIgnoreError ? 'force:eslint' : 'eslint';
     if (eslint) {
       if (filesToProcess(eslint)) {
-        validate.push('eslint:validate');
+        validate.push(eslintName + ':validate');
       }
     }
 
@@ -213,11 +213,13 @@ module.exports = function(grunt) {
         analyze.push('phpcs:analyze');
       }
     }
-    var eslint = grunt.config.get('eslint.analyze');
+    var eslint = grunt.config.get('eslint.analyze'),
+      eslintIgnoreError = grunt.config.get('config.validate.ignoreError') === undefined ? false : grunt.config.get('config.validate.ignoreError'),
+      eslintName = eslintIgnoreError ? 'force:eslint' : 'eslint';
     if (eslint) {
       // The eslint:analyze task has a deeper configuration structure than eslint:validate.
       if (filesToProcess(eslint.src)) {
-        analyze.push('eslint:analyze');
+        analyze.push(eslintName + ':analyze');
       }
     }
 
