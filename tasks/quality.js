841bff737b6db112620e81f86b6db6380244c04c
Grayside <aross@phase2technology.com>
2016-03-02 12:26:08 -0800

    Skip *.panels_default.inc in phplint checks.

--- tasks/quality.js
+++ tasks/quality.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
 
   var defaultPatterns = [
     '<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.{php,module,inc,install,profile}',
-    '!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.{box,pages_default,views_default,panelizer,strongarm}.inc',
+    '!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.{box,pages_default,panels_default,views_default,panelizer,strongarm}.inc',
     '!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.features.*inc',
     '!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/vendor/**'
   ];
