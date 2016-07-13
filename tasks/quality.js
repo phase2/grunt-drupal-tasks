82b5bc84988b961586085f5a7b78179bc252ddb4
Joe Turgeon <arithmetric@gmail.com>
2016-07-13 18:02:28 -0500

    Adding yml file validation to phpcs task only. Adding yml file with invalid format to D8 test assets.

--- tasks/quality.js
+++ tasks/quality.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
   var analyze = [];
 
   var defaultPatterns = [
-    '<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.{php,module,inc,install,profile,yml}',
+    '<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.{php,module,inc,install,profile}',
     '!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.{box,pages_default,panels_default,views_default,panelizer,strongarm}.inc',
     '!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.features.*inc',
     '!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/vendor/**'
@@ -49,6 +49,7 @@ module.exports = function(grunt) {
 
   // Exclude templates by default from phpcs validation.
   var phpcsPatterns = defaultPatterns.slice(0);
+  phpcsPatterns.unshift('<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.yml');
   phpcsPatterns.push('!<%= config.srcPaths.drupal %>/{modules,profiles,libraries,static}/**/*.tpl.php');
 
   var phpcsConfig = grunt.config.get('config.phpcs');
