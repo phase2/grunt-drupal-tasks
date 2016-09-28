ac0be80d84cabe3d80ce37875d03cf433b1e048f
Mike Potter <mpotter@phase2technology.com>
2016-09-28 14:27:06 +0000

    Put docker files in right place

--- docs/Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.4
-
-RUN pip install mkdocs
-
-RUN mkdir /documents
-WORKDIR /documents
-
-CMD mkdocs build --clean --site-dir /site
--- docs/build.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/bash
-# Script to build mkdocs site if mkdocs is installed locally
-#
-mkdocs build --clean --site-dir ../../phase2.github.io/grunt-drupal-tasks
