3cc622a050ccc6c84cbc1a68aed26b24a0607930
Mike Potter <mpotter@phase2technology.com>
2016-10-04 15:34:11 +0000

    Revert "Add options to package task to exclude paths such as node_modules and bower_components"
    This reverts commit 3bf8f4e2f092264360d4933626dee3ce7e334bad.

--- tasks/package.js
+++ tasks/package.js
@@ -66,20 +66,6 @@ module.exports = function(grunt) {
     var srcFiles = ['**', '!**/.gitkeep'].concat((config && config.srcFiles && config.srcFiles.length) ? config.srcFiles : '**');
     var projFiles = (config && config.projFiles && config.projFiles.length) ? config.projFiles : [];
 
-    var exclude = grunt.config('config.packages.exclude');
-    if (exclude !== false) {
-      var excludePaths = grunt.config('config.packages.excludePaths');
-      if (!excludePaths || excludePaths.length == 0) {
-        excludePaths = ['bower_components', 'node_modules'];
-      }
-
-      for (var key in excludePaths) {
-        excludePaths[key] = '!**/' + excludePaths[key] + '/**';
-      }
-      srcFiles = srcFiles.concat(excludePaths);
-      projFiles = projFiles.concat(excludePaths);
-    }
-
     // Look for a package target spec, build destination path.
     var packageName = grunt.option('name') || config.name || 'package';
     var destPath = grunt.config.get('config.buildPaths.packages') + '/' + packageName;
