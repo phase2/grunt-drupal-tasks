a056f0a770b088e2224d9e634dde8a2b75275d7e
Greg Garvey <ggarvey@stanford.edu>
2018-10-09 09:27:24 -0700

    added symlink to deployment

--- soe/Gruntfile.js
+++ soe/Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
   // Load all plugins and tasks defined by the grunt-drupal-tasks package.
   require('grunt-drupal-tasks')(grunt);
   grunt.loadNpmTasks('grunt-deploy-site');
+  grunt.loadNpmTasks('grunt-symlink');
   grunt.config.set('deploy_site.development.options.branch', 'SOE-develop-mc');
   grunt.config.set('deploy_site.development.options.commit_msg', 'deployment');
   grunt.config.set('deploy_site.development.base_path', 'build/packages/package');
@@ -10,5 +11,8 @@ module.exports = function(grunt) {
   grunt.config.set('copy.acquia_dependencies.cwd', 'acquia_dependencies/');
   grunt.config.set('copy.acquia_dependencies.src', '*/**');
   grunt.config.set('copy.acquia_dependencies.dest', 'build/packages/package/');
-  grunt.registerTask('git_deploy', ['copy:acquia_dependencies', 'deploy_site']);
+  grunt.config.set('symlink.simplesaml_symlink.dest', 'build/packages/package/docroot/simplesaml');
+  grunt.config.set('symlink.simplesaml_symlink.relativeSrc', '../simplesamlphp/www');
+  grunt.config.set('symlink.simplesaml_symlink.options', {type: 'dir'});
+  grunt.registerTask('git_deploy', ['copy:acquia_dependencies', 'symlink:simplesaml_symlink', 'deploy_site']);
 };
