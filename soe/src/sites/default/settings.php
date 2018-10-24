b6c8f6501044ed255e67e9963471141c1f56f98f
Greg Garvey <ggarvey@stanford.edu>
2018-10-24 10:41:35 -0700

    forcing https in settings

--- soe/src/sites/default/settings.php
+++ soe/src/sites/default/settings.php
@@ -52,6 +52,18 @@
  * @see conf_path()
  */
 
+// Require HTTPS.
+// We're behind a load-balancer, so we can't check $_SERVER['HTTPS'].
+// Have to check HTTP_X_FORWARDED_PROTO.
+// $_SERVER['HTTP_X_FORWARDED_PROTO'] is only set when we're serving over https,
+// therefore check if it's set.
+if (!array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER) && !drupal_is_cli()) {
+  header('HTTP/1.0 301 Moved Permanently');
+  $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  header("Location: " . check_plain($redirect_url));
+  drupal_exit();
+}
+
 /**
  * Database settings:
  *
